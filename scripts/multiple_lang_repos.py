bf8733dfcb1404baf7dfacbc2b79ab9f885ecd06
ginacassin <ginacassin47@gmail.com>
2023-12-17 13:18:19 +0100

    refactor: multiple lang script

--- scripts/multiple_lang_repos.py
+++ scripts/multiple_lang_repos.py
@@ -1,62 +1,69 @@
+import argparse
 from script_interface import ScriptInterface
 from pyspark.sql import functions as f
 from pyspark.sql.types import ArrayType, StringType
 from itertools import combinations
 
+
 class MultiLanguages(ScriptInterface):
     def __init__(self):
-        super().__init__('Top 5 multi-language repositories')
+        super().__init__('Most used language combinations in repositories by a specific language', language=True)
+        self.language_filter = self.parser.parse_args().language
+        self.log.info('Filtering by language: %s', self.language_filter)
 
-    def multi_language_stats(self):
+    def multi_languages(self):
         """
         Works with the 'languages' table.
-        Gets statistics on repositories with more than one language.
-        :return: Spark dataframe with multi-language statistics.
+        Obtain the combination of languages by input language, ordered by count in descending order.
+        :return: 3 Spark dataframe with multi-language statistics.
         """
         # Obtain 'languages' table
         languages_df = self.get_table('languages')
 
         # Convert the 'language_name' column to an array of strings
-        languages_df = languages_df.withColumn('language_name', f.from_json('language_name', ArrayType(StringType())))
+        filtered_languages_df = languages_df.withColumn('language_name',
+                                                        f.from_json('language_name', ArrayType(StringType())))
 
-        # Check if the array has more than one language, filter repositories with more than one language
-        multi_language_repos_df = languages_df.filter(f.size('language_name') > 1)
+        # Filter only repositories with more than one language
+        multi_language_repos_df = filtered_languages_df.filter(f.size('language_name') > 1)
 
         # Count the number of repositories with more than one language
         total_multi_language_repos = multi_language_repos_df.count()
 
         # Calculate the average number of languages per repository
-        avg_languages_per_repo = languages_df.agg(f.avg(f.size('language_name')).alias('avg_languages')).collect()[0]['avg_languages']
+        avg_languages_per_repo = \
+            filtered_languages_df.agg(f.avg(f.size('language_name')).alias('avg_languages')).collect()[0][
+                'avg_languages']
 
         # Calculate the combinations of languages used in repositories
-        language_combinations = multi_language_repos_df.withColumn('language_combinations', f.udf(lambda langs: list(combinations(sorted(langs), 2)), ArrayType(ArrayType(StringType())))('language_name'))
+        language_combinations = multi_language_repos_df.filter(f.array_contains('language_name', self.language_filter))
 
-        # Flatten the combinations and count occurrences
-        language_combination_counts = language_combinations.select(f.explode('language_combinations').alias('language_combination')).groupBy('language_combination').count()
+        # Count occurrences of each combination
+        language_combination_counts = language_combinations.groupBy('language_name').count()
 
-        # Order by count in descending order
-        sorted_combinations = language_combination_counts.orderBy('count', ascending=False)
+        # Order by count in descending order. Show only the top 25 combinations
+        sorted_combinations = language_combination_counts.orderBy('count', ascending=False).limit(25)
 
-        # Select the top 5 language combinations
-        top_combinations = sorted_combinations.limit(5)
-
-        return total_multi_language_repos, avg_languages_per_repo, top_combinations
+        return total_multi_language_repos, avg_languages_per_repo, sorted_combinations
 
     def process_data(self):
         # Obtain multi-language statistics
-        total_multi_language_repos, avg_languages_per_repo, top_combinations = self.multi_language_stats()
+        total_multi_language_repos, avg_languages_per_repo, sorted_combinations = self.multi_languages()
 
         # Create a DataFrame with the results
-        result_df = self.spark.createDataFrame([(total_multi_language_repos, avg_languages_per_repo)], ["total_multi_language_repos", "avg_languages_per_repo"]).crossJoin(top_combinations)
+        stats_df = self.spark.createDataFrame([(total_multi_language_repos, avg_languages_per_repo)],
+                                              ["total_multi_language_repos", "avg_languages_per_repo"])
 
         # Log and print the combined result (if in test mode)
         if self.test_mode:
-            result_df.show(truncate=False)
-            self.log.info('Combined result: \n%s', result_df.toPandas())
+            stats_df.show(truncate=False)
+            sorted_combinations.show(n=25, truncate=False)
+            self.log.info('Stats: \n%s', stats_df.toPandas())
+            self.log.info('Language combinations for %s: \n%s', self.language_filter, sorted_combinations.toPandas())
 
         # Save the combined result to a CSV file
-        self.save_data(result_df, 'mul_lang_result')
-
+        self.save_data(stats_df, 'mul_lang_stats')
+        self.save_data(sorted_combinations, 'mul_lang_combinations_' + self.language_filter)
 
 
 if __name__ == "__main__":
